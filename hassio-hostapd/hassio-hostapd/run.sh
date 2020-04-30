b6fcb1757f35c866742391b25b85d4b7870a71ce
ievren <kasperlifan@gmail.com>
2020-04-30 18:54:55 +0200

    Update run.sh

--- hassio-hostapd/hassio-hostapd/run.sh
+++ hassio-hostapd/hassio-hostapd/run.sh
@@ -9,14 +9,14 @@ ADDRESS=$(jq --raw-output ".address" $CONFIG_PATH)
 NETMASK=$(jq --raw-output ".netmask" $CONFIG_PATH)
 BROADCAST=$(jq --raw-output ".broadcast" $CONFIG_PATH)
 HIDEAP=$(jq --raw-output ".hide_ap" $CONFIG_PATH)
-DEVICE=$(jq --raw-output ".device" $CONFIG_PATH)
+INTERFACE=$(jq --raw-output ".interface" $CONFIG_PATH)
 
 # SIGTERM-handler this funciton will be executed when the container receives the SIGTERM signal (when stopping)
 term_handler(){
 	echo "Stopping..."
-	ifdown DEVICE
-	ip link set DEVICE down
-	ip addr flush dev DEVICE
+	ifdown INTERFACE
+	ip link set INTERFACE down
+	ip addr flush dev INTERFACE
 	exit 0
 }
 
@@ -26,10 +26,10 @@ trap 'term_handler' SIGTERM
 echo "Starting..."
 
 echo "Set nmcli managed no"
-nmcli dev set DEVICE managed no
+nmcli dev set INTERFACE managed no
 
 # Enforces required env variables
-required_vars=(SSID WPA_PASSPHRASE CHANNEL ADDRESS NETMASK BROADCAST)
+required_vars=(SSID WPA_PASSPHRASE CHANNEL ADDRESS NETMASK BROADCAST INTERFACE)
 for required_var in "${required_vars[@]}"; do
     if [[ -z ${!required_var} ]]; then
         error=1
@@ -41,8 +41,12 @@ if [[ -n $error ]]; then
     exit 1
 fi
 
+#Setup interface
+echo "iface $INTERFACE inet static"$'\n' >> /interfaces
+
 # Setup hostapd.conf
 echo "Setup hostapd ..."
+echo "interface=$INTERFACE"$'\n' >> /hostapd.conf
 echo "ssid=$SSID"$'\n' >> /hostapd.conf
 echo "wpa_passphrase=$WPA_PASSPHRASE"$'\n' >> /hostapd.conf
 echo "channel=$CHANNEL"$'\n' >> /hostapd.conf
@@ -55,17 +59,17 @@ fi
 # Setup interface
 echo "Setup interface ..."
 
-#ip link set DEVICE down
-#ip addr flush dev DEVICE
-#ip addr add ${IP_ADDRESS}/24 dev DEVICE
-#ip link set DEVICE up
+#ip link set INTERFACE down
+#ip addr flush dev INTERFACE
+#ip addr add ${IP_ADDRESS}/24 dev INTERFACE
+#ip link set INTERFACE up
 
 echo "address $ADDRESS"$'\n' >> /etc/network/interfaces
 echo "netmask $NETMASK"$'\n' >> /etc/network/interfaces
 echo "broadcast $BROADCAST"$'\n' >> /etc/network/interfaces
 
-ifdown DEVICE
-ifup DEVICE
+ifdown INTERFACE
+ifup INTERFACE
 
 echo "Starting HostAP daemon ..."
 hostapd -d /hostapd.conf & wait ${!}
